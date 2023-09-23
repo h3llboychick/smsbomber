3a5e1211489a14ff8f829e492598c3cd372a6b4e
h3llboychick <60476646+h3llboychick@users.noreply.github.com>
2023-09-23 12:27:21 +0600

    dom.kz added

---
+++ services/dom.py
@@ -0,0 +1,12 @@
+from .base import Service
+
+class Dom(Service):
+	timeout = 180
+	name = "dom.kz"
+	allowed_statuses = [200,300]
+	async def send_one(self, phone, session):
+		data = {"phone": self.format_number('+77xxxxxxxxx', phone)}
+		return await self.make_request(session, url = "https://api.dom.kz/api/auth/phone", method = "post", data = json)
+
+if __name__ == "__main__":
+	Dom.test()

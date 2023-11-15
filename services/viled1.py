e90bc8e88a6db8f3987c047e692c965623a73ed8
h3llboychick <60476646+h3llboychick@users.noreply.github.com>
2023-11-15 10:05:59 +0600

    viled1 added

---
+++ services/viled1.py
@@ -0,0 +1,12 @@
+from .base import Service
+
+class Viled1(Service):
+	timeout = 120
+	name = "viled1"
+	allowed_statuses = [200]
+	async def send_one(self, phone, session):
+		
+		return await self.make_request(session, url = f"https://api-prod.viled.kz/tizilimer/api/v1/users/sms?phone={phone}", method = "get")
+
+if __name__ == "__main__":
+	Viled1.test()

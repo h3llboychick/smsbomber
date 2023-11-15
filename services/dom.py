606bf42151e4ea653f0777bc946445dbbdde55db
h3llboychick <60476646+h3llboychick@users.noreply.github.com>
2023-11-15 10:25:26 +0600

    Update dom.py

--- services/dom.py
+++ services/dom.py
@@ -6,7 +6,7 @@ class Dom(Service):
 	allowed_statuses = [200,300]
 	async def send_one(self, phone, session):
 		data = {"phone": self.format_number('+77xxxxxxxxx', phone)}
-		return await self.make_request(session, url = "https://api.dom.kz/api/auth/phone", method = "post", data = json)
+		return await self.make_request(session, url = "https://api.dom.kz/api/auth/phone", method = "post", json = data)
 
 if __name__ == "__main__":
 	Dom.test()

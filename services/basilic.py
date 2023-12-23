8005f743c07cc6f507c79147fcb7afdca1e939ba
h3llboychick <60476646+h3llboychick@users.noreply.github.com>
2023-12-23 11:12:37 +0600

    Update basilic.py

--- services/basilic.py
+++ services/basilic.py
@@ -6,8 +6,7 @@ class Basilic(Service):
 	allowed_statuses = [200]
 	async def send_one(self, phone, session):
 		data = {"is_registration": true, "name": "sdsdsds", "phone": self.format_number('77xxxxxxxxx', phone)}
-		return await self.make_request(session, url = '
-https://api.basilic.kz/api/auth/send_confirmation', method = "post", data = json)
+		return await self.make_request(session, url = 'https://api.basilic.kz/api/auth/send_confirmation', method = "post", data = json)
 
 if __name__ == "__main__":
 	Basilic.test()
